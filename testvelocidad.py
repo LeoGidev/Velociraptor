ffd66707ef168223d0bae3c10463cf13ae3709d8
LeoGidev <leogimenez.dev@gmail.com>
2023-12-11 15:11:23 -0300

    se hace funcion Test con la cual se hace la prueba

--- testvelocidad.py
+++ testvelocidad.py
@@ -5,3 +5,20 @@ import tkinter
 from tkinter import*
 from tkinter import ttk
 
+def test():
+    st = speedtest.Speedtest()
+    d_st = st.download()/1000000
+    dwnT = round(d_st,2)
+    u_st = st.upload()/1000000
+    upldT = round(u_st,2)
+    print("tu velocidad es", dwnT, "Mbs")
+    print("tu subida es", upldT, "Mbs")
+    st.get_servers([])
+    ping = st.results.ping
+    print("tu ping es de", ping)
+    compDwn= 'descarga: ' + str(dwnT) + ' mbs \n ' + 'subida: ' + str(upldT) + ' mbs \n ping: ' + str(ping)
+    #requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': 'Resultado de la prueba:'})
+    #requests.post('https://api.telegram.org/[BOTTOKEN]/sendMessage',data={'chat_id': '[CHATID]', 'text': compDwn})
+    mensaje.set(compDwn)
+    mensaje2.set('listo')
+
